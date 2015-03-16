95e33b6e898a1fba035598d44497e71dcad4c072
Devi Menon <devi.menon@tstllc.net>
2015-03-15 21:41:59 -0400

    removed unnecessary json format in ElemicaUser

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -2,6 +2,6 @@ package models
 
 import sorm._
 
-object DB  extends Instance(entities = Seq(Entity[ElemicaUser]()),url ="jdbc:h2:mem:test", initMode = InitMode.Create)
+object DB  extends Instance(entities = Seq(Entity[ElemicaUser]()), url ="jdbc:h2:mem:test", initMode = InitMode.Create)
 
 
--- app/models/ElemicaUser.scala
+++ app/models/ElemicaUser.scala
@@ -1,10 +1,4 @@
 package models
 
-import play.api.libs.json.Json
-
 
 case class ElemicaUser(first: String , last: String , email:String, password:String)
-
-object ElemicaUser {
-  implicit val userFormat = Json.format[ElemicaUser]
-}
